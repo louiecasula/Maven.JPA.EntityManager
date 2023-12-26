57efad84285ec3783347c240b75dc94ac856d3e6
louie <louiecasula@gmail.com>
2023-12-26 10:19:05 -0500

    Added constructors, getters and setters for WordInfo and Languages

--- src/main/java/entities/Languages.java
+++ src/main/java/entities/Languages.java
@@ -17,4 +17,52 @@ public class Languages {
     private String languageName;
     @Column(name = "language_family")
     private String languageFamily;
+
+    public Languages(Long id, Set<WordInfo> word, String languageName, String languageFamily) {
+        this.id = id;
+        this.word = word;
+        this.languageName = languageName;
+        this.languageFamily = languageFamily;
+    }
+
+    public Languages(Set<WordInfo> word, String languageName, String languageFamily) {
+        this.word = word;
+        this.languageName = languageName;
+        this.languageFamily = languageFamily;
+    }
+
+    public Languages() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Set<WordInfo> getWord() {
+        return word;
+    }
+
+    public void setWord(Set<WordInfo> word) {
+        this.word = word;
+    }
+
+    public String getLanguageName() {
+        return languageName;
+    }
+
+    public void setLanguageName(String languageName) {
+        this.languageName = languageName;
+    }
+
+    public String getLanguageFamily() {
+        return languageFamily;
+    }
+
+    public void setLanguageFamily(String languageFamily) {
+        this.languageFamily = languageFamily;
+    }
 }
--- src/main/java/entities/WordInfo.java
+++ src/main/java/entities/WordInfo.java
@@ -26,5 +26,81 @@ public class WordInfo {
     @Column(name = "etymology")
     private String etymology;
 
+    public WordInfo(Long id, Languages language, String word, String definition, String exampleSentence, String pronunciation, String etymology) {
+        this.id = id;
+        this.language = language;
+        this.word = word;
+        this.definition = definition;
+        this.exampleSentence = exampleSentence;
+        this.pronunciation = pronunciation;
+        this.etymology = etymology;
+    }
 
+    public WordInfo(Languages language, String word, String definition, String exampleSentence, String pronunciation, String etymology) {
+        this.language = language;
+        this.word = word;
+        this.definition = definition;
+        this.exampleSentence = exampleSentence;
+        this.pronunciation = pronunciation;
+        this.etymology = etymology;
+    }
+
+    public WordInfo() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Languages getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(Languages language) {
+        this.language = language;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String getDefinition() {
+        return definition;
+    }
+
+    public void setDefinition(String definition) {
+        this.definition = definition;
+    }
+
+    public String getExampleSentence() {
+        return exampleSentence;
+    }
+
+    public void setExampleSentence(String exampleSentence) {
+        this.exampleSentence = exampleSentence;
+    }
+
+    public String getPronunciation() {
+        return pronunciation;
+    }
+
+    public void setPronunciation(String pronunciation) {
+        this.pronunciation = pronunciation;
+    }
+
+    public String getEtymology() {
+        return etymology;
+    }
+
+    public void setEtymology(String etymology) {
+        this.etymology = etymology;
+    }
 }
