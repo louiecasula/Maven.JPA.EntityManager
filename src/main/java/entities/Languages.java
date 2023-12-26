463b9295bb8d8d8a0761dd3be2758a6db0fe02e6
louie <louiecasula@gmail.com>
2023-12-26 09:37:43 -0500

    Set up entities and their respective annotations including relationship to one another

--- src/main/java/entities/Languages.java
+++ src/main/java/entities/Languages.java
@@ -2,9 +2,19 @@ package entities;
 
 import jakarta.persistence.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 public class Languages {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "language_id")
     private Long id;
+    @OneToMany(mappedBy = "language")
+    private Set<WordInfo> word = new HashSet<>();
+    @Column(name = "language_text")
+    private String languageName;
+    @Column(name = "language_family")
+    private String languageFamily;
 }
--- src/main/java/entities/WordInfo.java
+++ src/main/java/entities/WordInfo.java
@@ -2,9 +2,29 @@ package entities;
 
 import jakarta.persistence.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
+@Table
 public class WordInfo {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "word_id")
     private Long id;
+    @ManyToOne
+    @JoinColumn(name = "language_id")
+    private Languages language;
+    @Column(name = "word")
+    private String word;
+    @Column(name = "definition")
+    private String definition;
+    @Column(name = "exampleSentence")
+    private String exampleSentence;
+    @Column(name = "pronunciation")
+    private String pronunciation;
+    @Column(name = "etymology")
+    private String etymology;
+
+
 }
